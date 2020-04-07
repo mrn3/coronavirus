6782b3ee645c26c072afd60679a1ade710eeb2cb
Matt Newman <Matthew.Newman@coxautoinc.com>
2020-04-07 07:04:34 -0600

    build

---
+++ client/build/precache-manifest.3bfca6b0553fdb89b729a15ab8eaa66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db5211cdb9464df806a9ed808f500d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0973a4f395f40542c0c1",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "c1fcd2b059b97fe9e1cc",
+    "url": "/static/js/2.7a667933.chunk.js"
+  },
+  {
+    "revision": "0973a4f395f40542c0c1",
+    "url": "/static/js/main.bc829e06.chunk.js"
+  },
+  {
+    "revision": "f0d7277440c940f96e5d",
+    "url": "/static/js/runtime~main.9120247e.js"
+  },
+  {
+    "revision": "32f54de70f656b326bc9fe534021320a",
+    "url": "/static/media/background.32f54de7.png"
+  },
+  {
+    "revision": "f28de6c4c5dd8682c21c2b32ea0dc817",
+    "url": "/static/media/coronavirus_logo.f28de6c4.png"
+  }
+]);
